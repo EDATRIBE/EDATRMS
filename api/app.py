8f788ae49b55ac50c04e332ce9e576bd2f09fb31
Amaindex <amaindex@outlook.com>
2021-05-17 17:54:02 +0800

    enrich frame

--- api/app.py
+++ api/app.py
@@ -2,3 +2,47 @@ import os
 
 from sanic import Sanic
 from sanic_session import Session, AIORedisSessionInterface
+
+from .config import config, log_config
+from .models import init_db, close_db, init_cache, close_cache
+from .blueprints import handle_exception
+
+os.makedirs(config['DATA_PATH'], 0o755, True)
+
+app = Sanic(config['NAME'].capitalize(), log_config=log_config)
+app.config.update(config)
+
+app.error_handler.add(Exception, handle_exception)
+
+app.static('/image', os.path.join(config['DATA_PATH'], config['IMAGE_DIR']),
+           stream_large_files=True)
+
+
+@app.listener('before_server_start')
+async def server_init(app, loop):
+    app.db = await init_db(config)
+    app.cache = await init_cache(config)
+
+    Session(
+        app,
+        AIORedisSessionInterface(
+            app.cache, expiry=config['SESSION_EXPIRY']
+        )
+    )
+
+
+@app.listener('after_server_stop')
+async def server_clean(app, loop):
+    await close_cache(app.cache)
+    await close_db(app.db)
+
+
+if __name__ == '__main__':
+    app.run(
+        host=config['HOST'],
+        port=config['PORT'],
+        debug=config['DEBUG'],
+        auto_reload=config['AUTO_RELOAD'],
+        access_log=config['ACCESS_LOG'],
+        workers=config['WORKERS']
+    )
