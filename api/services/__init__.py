f1f3922cb2f4e5403ee6c610a78771a077dffed1
Amaindex <amaindex@outlook.com>
2021-05-31 07:04:07 +0800

    init storage service

--- api/services/__init__.py
+++ api/services/__init__.py
@@ -1,2 +1,12 @@
 from .common import ServiceException
+
+from .ip import IPService
+from .animation import AnimationService
+from .video import VideoService
+from .caption import CaptionService
+from .novel import NovelService
+from .storage import StorageService
+from .tag import TagService
+from .ip_tag import IPTagService
 from .user import UserService
+from .caption_user import CaptionUserService
