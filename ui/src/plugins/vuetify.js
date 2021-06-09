7f98d94c6d7f3f6aea88cebd1e8b5d7ad249f98f
Amaindex <amaindex@outlook.com>
2021-06-09 22:43:01 +0800

    init ui frame
    include dependence and theme preset

--- ui/src/plugins/vuetify.js
+++ ui/src/plugins/vuetify.js
@@ -1,7 +1,40 @@
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: false,
+        default: 'light',
+        disable: false,
+        options: {
+            cspNonce: undefined,
+            customProperties: undefined,
+            minifyTheme: undefined,
+            themeCache: undefined,
+        },
+        themes: {
+            light: {
+                primary: '#0366D6',
+                secondary: '#24292E',
+                accent: '#82B1FF',
+                error: '#FF5252',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FB8C00',
+            },
+            dark: {
+                primary: '#2196F3',
+                secondary: '#424242',
+                accent: '#FF4081',
+                error: '#FF5252',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FB8C00',
+            }
+        }
+    }
 });
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:7000',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
