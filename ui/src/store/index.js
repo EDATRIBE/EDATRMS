feb2c47a24f3cef3506f7e09cb80446bda29229e
Amaindex <amaindex@outlook.com>
2021-06-20 22:32:09 +0800

    init vuex

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,29 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import example from './module-example'
-
 Vue.use(Vuex)
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export default function (/* { ssrContext } */) {
-  const Store = new Vuex.Store({
-    modules: {
-      // example
-    },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    strict: process.env.DEBUGGING
-  })
-
-  return Store
-}
+export default new Vuex.Store({
+  state: {
+    user: null
+  },
+  mutations: {
+    setUser(state,user){
+      state.user=user
+    }
+  }
+})
