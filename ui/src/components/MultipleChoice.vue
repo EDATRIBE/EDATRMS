f7fab8af55d7b54836d0635d55a6124640551085
Amaindex <amaindex@outlook.com>
2021-07-02 17:40:39 +0800

    dev MultipleChoice component

---
+++ ui/src/components/MultipleChoice.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row">
+    <q-btn
+      size="0.75em" flat class="no-border-radius"
+      v-for="(opt,i) in options" :key="'opt'+i"
+      @click="choice(i)"
+      :class="[
+        opt.value===value?activeBGClass:BGClass,
+        opt.value===value?activeTextClass:textClass
+      ]"
+    >
+      {{ opt.label }}
+    </q-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultipleChoice",
+  model: {
+    prop: 'value',
+    event: 'choice'
+  },
+  props: {
+    value: String,
+    options: Array,
+    textClass: String,
+    activeTextClass: String,
+    BGClass: String,
+    activeBGClass: String
+  },
+  methods: {
+    choice(i) {
+      this.$emit('choice', this.options[i].value)
+    }
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
