ec3c0d118bcc083427120088af5139f8d08d840c
Amaindex <amaindex@outlook.com>
2021-07-17 12:31:42 +0800

    init component 'IPDeleteDialog'

---
+++ ui/src/layout_pages/IP/IPDeleteDialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-dialog v-model="show">
+    <div class="bg-dark column q-pa-lg " style="width: 60vw; max-width: 60vw;">
+      <q-btn v-close-popup>hello</q-btn>
+    </div>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: "IPDeleteDialog",
+  props: {
+    isDeleting: Boolean,
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+  },
+  watch: {
+    isDeleting(){
+      this.show=this.isDeleting
+    },
+    show(){
+      this.$emit('update:isDeleting',this.show)
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
