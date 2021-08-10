f5604f42698fc2df46cd1369246cf00eb05cfdd5
Amaindex <amaindex@outlook.com>
2021-08-10 09:41:55 +0800

    fix a heisenbug

--- ui/src/layout_pages/Index/Index.vue
+++ ui/src/layout_pages/Index/Index.vue
@@ -2,61 +2,59 @@
     <q-page class="q-px-md q-pb-xl" style="padding-top: 3.5em" v-if="!LD">
         <q-scroll-observer @scroll="onScroll"/>
         <div class="q-mx-auto" style="width: 97%">
-            <div class="column">
-                <!--SEARCH-->
-                <q-input
-                    dense dark class="text-h5 bg-dark-light q-mb-lg" style="width: 100%" standout=""
-                    v-model="searchBuffer"
-                    debounce="300"
-                    autocomplete="false"
+            <!--SEARCH-->
+            <q-input
+                dense dark class="text-h5 bg-dark-light q-mb-lg" style="width: 100%" standout=""
+                v-model="searchBuffer"
+                debounce="300"
+                autocomplete="false"
+            >
+                <template v-slot:append>
+                    <q-spinner-grid
+                        v-show="searching"
+                        color="dark"
+                    />
+                </template>
+            </q-input>
+            <!--TABS-->
+            <q-tabs
+                style="width: 100%"
+                v-model="tab"
+                class="text-grey"
+                align="justify"
+                inline-label
+                dense
+                outside-arrows
+            >
+                <q-route-tab
+                    ripple class="text-primary text-weight-medium" name="Animations" style="width: 50%"
+                    to="/index/animations"
                 >
-                    <template v-slot:append>
-                        <q-spinner-grid
-                            v-show="searching"
-                            color="dark"
-                        />
-                    </template>
-                </q-input>
-                <!--TABS-->
-                <q-tabs
-                    style="width: 100%"
-                    v-model="tab"
-                    class="text-grey"
-                    align="justify"
-                    inline-label
-                    dense
-                    outside-arrows
+                    <q-icon class="q-mr-xs" size="1.7em" name="movie"></q-icon>
+                    {{ $t('ui.index.animations') }}
+                </q-route-tab>
+                <q-route-tab
+                    ripple class="text-secondary text-weight-medium" name="Novels" style="width: 50%"
+                    to="/index/novels"
                 >
-                    <q-route-tab
-                        ripple class="text-primary text-weight-medium" name="Animations" style="width: 50%"
-                        to="/index/animations"
-                    >
-                        <q-icon class="q-mr-xs" size="1.7em" name="movie"></q-icon>
-                        {{ $t('ui.index.animations') }}
-                    </q-route-tab>
-                    <q-route-tab
-                        ripple class="text-secondary text-weight-medium" name="Novels" style="width: 50%"
-                        to="/index/novels"
-                    >
-                        <q-icon class="q-mr-xs" size="1.7em" name="import_contacts"></q-icon>
-                        {{ $t('ui.index.novels') }}
-                    </q-route-tab>
-                    <q-route-tab
-                        ripple class="text-accent text-weight-medium" name="IPsAndTags" style="width: 50%"
-                        v-if="currentUser&&currentUser.staff"
-                        to="/index/ips_and_tags"
-                    >
-                        <q-icon class="q-mr-xs" size="1.7em" name="source"></q-icon>
-                        {{ $t('ui.index.ips') }}
-                        <span class="q-mx-sm">|</span>
-                        <q-icon class="q-mr-xs" size="1.2em" name="fas fa-hashtag"></q-icon>
-                        {{ $t('ui.index.tags') }}
-                    </q-route-tab>
-                </q-tabs>
-                <keep-alive>
-                    <router-view/>
-                </keep-alive>
-            </div>
+                    <q-icon class="q-mr-xs" size="1.7em" name="import_contacts"></q-icon>
+                    {{ $t('ui.index.novels') }}
+                </q-route-tab>
+                <q-route-tab
+                    ripple class="text-accent text-weight-medium" name="IPsAndTags" style="width: 50%"
+                    v-if="currentUser&&currentUser.staff"
+                    to="/index/ips_and_tags"
+                >
+                    <q-icon class="q-mr-xs" size="1.7em" name="source"></q-icon>
+                    {{ $t('ui.index.ips') }}
+                    <span class="q-mx-sm">|</span>
+                    <q-icon class="q-mr-xs" size="1.2em" name="fas fa-hashtag"></q-icon>
+                    {{ $t('ui.index.tags') }}
+                </q-route-tab>
+            </q-tabs>
+            <keep-alive>
+                <router-view/>
+            </keep-alive>
         </div>
 
         <q-page-sticky expand position="top" class="bg-dark q-px-md q-py-sm" v-show="scrollInfo.position>112">
